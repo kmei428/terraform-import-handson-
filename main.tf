601e9a671e6f845ce2908f2bb1e8048a7b35bc2d
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-09-26 14:19:06 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform { 
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+  }
+  cloud { 
+    
+    organization = "{organization名}" 
+
+    workspaces { 
+      name = "{workspaces名}" 
+    } 
+  } 
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "imported" {
+  bucket = "sample-import-backet"
+}
